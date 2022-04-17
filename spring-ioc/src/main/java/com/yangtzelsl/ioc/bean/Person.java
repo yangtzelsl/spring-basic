2f45512027b68b66627bd04ea5ae5a813d5dc468
luis.liu <luis.liu@pkfare.com>
2022-04-17 22:51:47 +0800

    spring configuration annotation

---
+++ spring-ioc/src/main/java/com/yangtzelsl/ioc/bean/Person.java
@@ -0,0 +1,13 @@
+package com.yangtzelsl.ioc.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private Integer age;
+}
